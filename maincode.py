504e2b4ecd6a018112186a9c69ccd8d9bef5fa23
morelia-ma <l21330642@hermosillo.tecnm.mx>
2026-05-03 23:07:11 +0800

    Update maincode.py

--- maincode.py
+++ maincode.py
@@ -16,7 +16,7 @@ st.markdown("""
         border-left: 5px solid #0077B6; box-shadow: 2px 2px 8px rgba(0,0,0,0.08);
         height: 100px;
     }
-    .metric-title { font-size: 0.75rem; font-weight: bold; color: #666; text-transform: uppercase; }
+    .metric-title { font-size: 0.75rem; font-weight: 100; bold; color: #666; text-transform: uppercase; }
     .metric-value { font-size: 1.9rem; font-weight: 500; color: #1f2d3d; }
     .gas-wrapper { display: flex; flex-direction: column; align-items: center; justify-content: center; }
     .gas-container {
